ada7ff61f8db0e18db0efeb36dbdbcb9429546ec
wc453707244 <wc453707244@gmail.com>
2025-06-06 07:05:19 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "cmwc"         # 用户名
+UUID = "4e1aa169-e2e8-4a8c-b26d-7f163fa18efe"                     # UUID，留空则自动生成
+PORT = 65432                   # Vmess端口，留空或0则自动生成
+DOMAIN = "aa.cmwc.dpdns.org"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiYWU0OTIyM2I0ZDY2M2VkY2ZlYjVlOTA3ZDlmMWE2M2EiLCJ0IjoiYmQ1YzhkYzAtOThjZS00ZWI3LThjNzMtNDE4ZTQxNjEzNjExIiwicyI6IlptRXpNVGt5T1dZdFpEZGpZUzAwWXpVMExXSmtPRFV0T0ROak1tTTFZMkUwTkRNeiJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
